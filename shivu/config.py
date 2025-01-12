d6dd4d3d34ee2f1970d19b9485ad79cf9647f021
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2025-01-12 12:11:21 +0200

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,13 +2,13 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7011990425"
-    sudo_users = ["6890857225", "7011990425", "6264760837", "5527022252", "6384484443", "1850686769"]
+    OWNER_ID = "6965147961"
+    sudo_users = ["6864672519", "7710262210"]
     GROUP_ID = "-1002041586214"
-    TOKEN = "your bot token"
-    mongo_url = "your db"
+    TOKEN = "6501935889:AAHvx_2RNyfWQgF3kzGaSSn1E_35m7UszXs"
+    mongo_url = "mongodb+srv://soja11082:ZAWSN9qsi6nSBDrz@lust.6krav.mongodb.net/?retryWrites=true&w=majority&appName=lust"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
-    SUPPORT_CHAT = "lustsupport"
+    SUPPORT_CHAT = "https://t.me/+hVnMZwUzEpIyYmI1"
     UPDATE_CHAT = "lustxUpdate"
     BOT_USERNAME = "lustXcatcherrobot"
     CHARA_CHANNEL_ID = "-1002023474262"
